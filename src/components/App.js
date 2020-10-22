57418ef0ceccc658bcf24b0908c326de24754edb
NishantBadlani <nishant.03badlani@gmail.com>
2020-10-22 19:51:34 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
 import React, {Component, useState} from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <p data-ns-test="project-name">Football Website: News, scores, update and live stats</p>
+        <p data-ns-test="project-description">A website for football lovers with everthing they will ever need to stay update about the game.</p>
+      </>
+    );
+  }
 }
-
 
 export default App;
